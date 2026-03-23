2a3d75e6b9441933a490099657337a65f20e30da
Maleesha Amendra <72781485+Maleesh3038@users.noreply.github.com>
2026-03-23 22:46:29 +0530

    Add files via upload

---
+++ gusto/app.js
@@ -0,0 +1,7 @@
+const brand = document.querySelector(".brand");
+brand.computedStyleMap.color = "#d11f1f";
+
+const reservation = document.querySelector(".reservation");
+reservation.style.fontSize = "18px";
+reservation.style.textTranform = "uppercase";
+reservation.style.letterSpacing = "3px";
