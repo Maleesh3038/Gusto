7cd15a1028abaa3996d5af3c9ab61290d442358b
Maleesha Amendra <72781485+Maleesh3038@users.noreply.github.com>
2026-04-11 19:22:48 +0530

    Update app.js

--- gusto/app.js
+++ gusto/app.js
@@ -5,10 +5,3 @@ const reservation = document.querySelector(".reservation");
 reservation.style.fontSize = "18px";
 reservation.style.textTranform = "uppercase";
 reservation.style.letterSpacing = "3px";
-const brand = document.querySelector(".brand");
-brand.computedStyleMap.color = "#d11f1f";
-
-const reservation = document.querySelector(".reservation");
-reservation.style.fontSize = "18px";
-reservation.style.textTranform = "uppercase";
-reservation.style.letterSpacing = "3px";
